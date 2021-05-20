fc9776c0aa822c155907d8ba3d0c59c99c81ff6f
David Gee <cdhgee@users.noreply.github.com>
2021-05-20 15:09:22 -0500

    Add additional PSReadline options

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -81,11 +81,35 @@ $requiredVersion = New-Object System.Version(2, 2, 0)
 
 If ($psReadlineVersion.CompareTo($requiredVersion) -ge 0) {
 
-  Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView -HistorySearchCursorMovesToEnd
-  Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
-  Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+  Set-PSReadLineOption -PredictionSource History
+  Set-PSReadlineOption -PredictionViewStyle ListView
+  Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
-  Set-PSReadLineKeyHandler -Chord Alt+w -BriefDescription SaveInHistory -Description "Save in history but do not execute" -ScriptBlock {
+  # In Emacs mode - Tab acts like in bash, but the Windows style completion
+  # is still useful sometimes, so bind some keys so we can do both
+  Set-PSReadLineKeyHandler -Key Ctrl+q -Function TabCompleteNext
+  Set-PSReadLineKeyHandler -Key Ctrl+Q -Function TabCompletePrevious
+
+  # Clipboard interaction is bound by default in Windows mode, but not Emacs mode.
+  Set-PSReadLineKeyHandler -Key Ctrl+C -Function Copy
+  Set-PSReadLineKeyHandler -Key Ctrl+v -Function Paste
+
+  # CaptureScreen is good for blog posts or email showing a transaction
+  # of what you did when asking for help or demonstrating a technique.
+  Set-PSReadLineKeyHandler -Chord 'Ctrl+d,Ctrl+c' -Function CaptureScreen
+
+  # The built-in word movement uses character delimiters, but token based word
+  # movement is also very useful - these are the bindings you'd use if you
+  # prefer the token based movements bound to the normal emacs word movement
+  # key bindings.
+  Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
+  Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
+  Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
+  Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
+  Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
+  Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
+
+  Set-PSReadLineKeyHandler -Key Alt+w -BriefDescription SaveInHistory -Description "Save in history but do not execute" -ScriptBlock {
 
     $line = $null
     $cursor = $null
